27dcf5edec25568dab3cec8b291d26a3ed01dd10
JustNat <gabrielcp4@outlook.com>
2021-11-03 22:04:05 -0300

    adição do card e otimização da randomização

--- app/src/main/java/com/bruce/clickarael/MainActivity.kt
+++ app/src/main/java/com/bruce/clickarael/MainActivity.kt
@@ -2,7 +2,6 @@ package com.bruce.clickarael
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import com.bruce.clickarael.databinding.ActivityMainBinding
 import kotlin.random.Random
 
@@ -21,38 +20,9 @@ class MainActivity : AppCompatActivity() {
 
      private fun insertListeners() {
          binding.mainImage.setOnClickListener {
-             binding.clickTv.visibility = View.GONE
-             val random = Random.nextInt(9)
+             val random = Random.nextInt(10)
              val stringArray = resources.getStringArray(R.array.strings_random)
-             when {
-                 (random == 0) -> {
-                     binding.auxiliaryTv.text = stringArray[0]
-                 }
-                 (random == 1) -> {
-                     binding.auxiliaryTv.text = stringArray[1]
-                 }
-                 (random == 2) -> {
-                     binding.auxiliaryTv.text = stringArray[2]
-                 }
-                 (random == 3) -> {
-                     binding.auxiliaryTv.text = stringArray[3]
-                 }
-                 (random == 4) -> {
-                     binding.auxiliaryTv.text = stringArray[4]
-                 }
-                 (random == 5) -> {
-                     binding.auxiliaryTv.text = stringArray[5]
-                 }
-                 (random == 6) -> {
-                     binding.auxiliaryTv.text = stringArray[6]
-                 }
-                 (random == 7) -> {
-                     binding.auxiliaryTv.text = stringArray[7]
-                 }
-                 (random == 8) -> {
-                     binding.auxiliaryTv.text = stringArray[8]
-                 }
-             }
+             binding.clickTv.text = stringArray[random]
          }
      }
 }
